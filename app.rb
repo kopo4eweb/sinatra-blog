6b601ccb7a6db9081b109c79077aa1e93be0814f
kopo4eweb <kopo4e.web@yandex.ru>
2020-03-03 08:47:42 +0300

    init DB

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+  @db = SQLite3::Database.new 'blog.db'
+  @db.results_as_hash = true
+end
+
+before do
+  init_db
+end
 
 get '/' do
   erb "Hello, I'm Simple site!"
