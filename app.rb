e791db7c16d1ab228c9dfa13761190a9f317310e
kopo4eweb <kopo4e.web@yandex.ru>
2020-03-11 14:08:37 +0300

    detail page to post

--- app.rb
+++ app.rb
@@ -87,16 +87,14 @@ end
 get '/post/:id' do
   id = params[:id]
 
-  result = @db.execute 'SELECT * FROM Posts WHERE Id = ?', [id]
-  
-  @post = result[0]
+  @post = Post.find(id)
 
-  @comments = @db.execute 'SELECT * FROM Comments WHERE post_id = ? ORDER BY id', [id]
+  @comments = @post.comments
 
   if params[:error].to_i == 1
     @error = params[:comment] 
   end
- 
+  
   erb :post
 end
 
